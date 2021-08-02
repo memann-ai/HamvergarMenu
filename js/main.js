dfd1b6d4570238d85ecd1d6a57a721b509ca42c8
memann-ai <80218866+memann-ai@users.noreply.github.com>
2021-08-02 22:44:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@
     const overlay = document.querySelector(".overlay");
     const close = document.getElementById("close");
 
-    open.addEventListener('click', () => {
-        overlay.classList.add('show');
-        open.classList.add('hide'); //×アイコン下のmenuアイコン
+    open.addEventListener("click", () => {
+        overlay.classList.add("show");
+        open.classList.add("hide");
     });
     
-    close.addEventListener('click', () => {
-        overlay.classList.remove('show');
-        open.classList.remove('hide');
+    close.addEventListener("click", () => {
+        overlay.classList.remove("show");
+        open.classList.remove("hide");
     });
+
+    
 }
